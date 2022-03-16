0796d1981c5b28b95aa6da9a95088c8d52ee2bb6
mitasoar <49469777+mitasoar@users.noreply.github.com>
2022-03-16 17:06:44 +0900

    쇼핑몰 프로젝트
    Day_01

---
+++ Shop/src/main/java/com/kh/shop/controller/MemberController.java
@@ -0,0 +1,22 @@
+package com.kh.shop.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// @Controller 어노테이션의 역할
+// 1. 해당 클래스를 controller 인식
+// 2. 해당 클래스에 대한 객체를 생성
+@Controller
+@RequestMapping("/member") // - 요청 경로가 /member로 시작하면 MemberController 실행
+public class MemberController {
+}
